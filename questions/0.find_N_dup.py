18fe26e6e28e7a7d8828da874b8c2d7961b940ff
thieejn <thieejnphajm@gmail.com>
2019-03-06 02:28:00 +0700

    refactorng by default value

--- questions/0.find_N_dup.py
+++ questions/0.find_N_dup.py
@@ -8,19 +8,17 @@ def check_n_dup(str, n):
     tmp = {}
 
     for i in range(len(str)):
-        if tmp.has_key(str[i]):
-            tmp[str[i]] += 1
-            if tmp[str[i]] == n:
-                return i
-        else:
-            tmp[str[i]] = 1
+        tmp.setdefault(str[i], 0)
+        tmp[str[i]] += 1
+        if tmp[str[i]] == n:
+            return i
 
     return None;
 
 templates = string.letters
 
 str = ''.join(random.choice(string.letters) for _ in range(random.choice(range(20))))
-str = 'abcdabcabc'
+# str = 'abcdabcabc'
 
 print 'String: ', str
 
